afae02040aa98d1baaf9b8599a66aabcc127310a
Milous98 <Milous98@users.noreply.github.com>
2025-11-27 13:01:00 +0100

    Add projets

---
+++ Unit converter/script.js
@@ -0,0 +1,16 @@
+function convertNumber() {
+    let number = document.getElementById("number").value;
+    console.log(number);
+
+    choseCalc = document.getElementById("calcul").value;
+    // Miles to Km *1.60934
+    // Km to Miles *0.621371
+    let operation = 0;
+    if (choseCalc === "mTk") {
+        operation = 1.60934;
+    } else {
+        operation = 0.621371;
+    }
+    let resultat = document.querySelector("#result em");
+    resultat.innerHTML = number * operation;
+}
